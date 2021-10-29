9a2f38c47fb8a8ba1778baddae99d02990151acb
Katie Reed <ksmetz@email.unc.edu>
2021-10-29 09:58:41 -0400

    Added --mem
    Changed SLURM memory to a parameter (default 32g)

--- RNApipe.py
+++ RNApipe.py
@@ -190,6 +190,9 @@ runGroup.add_option("--signalout", "--sigout", "--SIGNALOUT", "--SIGOUT",action 
 # Rerun: option to overwrite existing files, rather than skip the steps that lead to them
 runGroup.add_option("--rerun", "--RERUN", action = "store", type = "string", dest = "rerun", 
                   help = "Toggle (T/F) whether to overwrite existing files in output directory.",default="T")
+# Mem: Select how much memory to run jobs with (default 32g)
+runGroup.add_option("--mem", "--MEM", action = "store", type = "string", dest = "mem", 
+                  help = "Memory for all SLURM jobs. (default=32g)",default="32g")
 
 parser.add_option_group(runGroup)
 
@@ -272,6 +275,7 @@ STAGE = optionLister(options.stage)
 SIGOUT = optionLister(options.sigout)
 MERGE = optionLister(options.merge)
 RERUN = options.rerun
+MEM = options.mem
 
 # Command parameters
 ANNT = options.annt
@@ -776,7 +780,7 @@ for n in range(len(config)):
         '#SBATCH -J CORE_' + nName + '\n' +
         '#SBATCH -n 8\n' +
         '#SBATCH -N 1\n' +
-        '#SBATCH --mem=32g \n' +
+        '#SBATCH --mem=' + MEM + '\n' +
         '#SBATCH -t 5760\n' +
         '#SBATCH -o ' + directories['debug'] + '/core_' + nName + '_' + stamp + '-%j.log.out\n' + 
         '#SBATCH -e ' + directories['debug'] + '/core_' + nName + '_' + stamp + '-%j.log.err\n' +
@@ -1006,7 +1010,7 @@ if "merge" in STAGE and mergeDF.shape[0] > 1:
                 '#SBATCH -J MERGE_' + mergeName + '\n' +
                 '#SBATCH -n 8\n' +
                 '#SBATCH -N 1\n' +
-                '#SBATCH --mem=32g \n' +
+                '#SBATCH --mem=' + MEM + '\n' +
                 '#SBATCH -t 5760\n' +
                 '#SBATCH -o ' + directories['debug'] + '/merge_' + mergeName + '_' + stamp + '-%j.log.out\n' + 
                 '#SBATCH -e ' + directories['debug'] + '/merge_' + mergeName + '_' + stamp + '-%j.log.err\n' +
@@ -1131,7 +1135,7 @@ if "QC" in STAGE or "quant" in STAGE:
         '#SBATCH -J FINAL_' + NAME + '\n' +
         '#SBATCH -n 8\n' +
         '#SBATCH -N 1\n' +
-        '#SBATCH --mem=32g \n' +
+        '#SBATCH --mem=' + MEM + '\n' +
         '#SBATCH -t 5760\n' +
         '#SBATCH -o ' + directories['debug'] + '/final_' + NAME + '_' + stamp + '-%j.log.out\n' + 
         '#SBATCH -e ' + directories['debug'] + '/final_' + NAME + '_' + stamp + '-%j.log.err\n' +
